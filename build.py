1af4ca218394e186dd525c70f70efb11fe7e0833
Stefan Neben <stefan.neben@gmail.com>
2015-10-21 17:31:33 +0200

    Package and upload function for the pybuilder crassus build

--- build.py
+++ build.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import boto3
+import zipfile
+
+from datetime import datetime
 from pybuilder.core import use_plugin, init, task
 
 use_plugin("python.core")
@@ -37,7 +46,6 @@ def set_properties(project):
 
 @init(environments='teamcity')
 def set_properties_for_teamcity_builds(project):
-    import os
     project.set_property('teamcity_output', True)
     project.version = '%s-%s' % (project.version,
                                  os.environ.get('BUILD_NUMBER', 0))
@@ -51,6 +59,48 @@ def set_properties_for_teamcity_builds(project):
                          os.environ.get('PYPIPROXY_URL'))
 
 
+# ------------------ AWS Lambda deployment logic starts here -------------------
+
+
+def zip_recursive(archive, directory, folder=""):
+    """Zip directories recursively"""
+    excludes = ['scripts']
+    for item in os.listdir(directory):
+        if not item in excludes:
+            if os.path.isfile(os.path.join(directory, item)):
+                archive.write(os.path.join(directory, item),
+                              os.path.join(folder, item),
+                              zipfile.ZIP_DEFLATED)
+            elif os.path.isdir(os.path.join(directory, item)):
+                zip_recursive(archive,
+                              os.path.join(directory, item),
+                              folder=os.path.join(folder, item))
+
+
+def copy_dir_content_to_ziproot(archive, directory):
+    """Put every file found in directory to root layer of the zipfile"""
+    #import pdb; pdb.set_trace()
+    for item in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, item)):
+            archive.write(os.path.join(directory, item),
+                          item, zipfile.ZIP_DEFLATED)
+
+
 @task
 def package_lambda_code():
-    print "HELLO WORLD!"
+    build_dir = 'target/dist/crassus-{0}/'.format(version)
+    archive = zipfile.ZipFile('target/crassus.zip', 'w')
+    zip_recursive(archive, build_dir)
+    copy_dir_content_to_ziproot(archive, '{0}/scripts/'.format(build_dir))
+    archive.close()
+
+
+@task
+def upload_zip_to_s3():
+    timestamp = time.time()
+    formatted_timestamp = datetime.fromtimestamp(timestamp)
+    formatted_timestamp = formatted_timestamp.strftime('%Y%m%d%H%M%S')
+    keyname = 'crassus-{0}.zip'.format(formatted_timestamp)
+    s3 = boto3.resource('s3')
+    data = open('target/crassus.zip', 'rb')
+    s3.Bucket('crassus-zips1').put_object(Key=keyname, Body=data)
