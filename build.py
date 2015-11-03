a011af6e347d6ce5e861357b6b5de009a413bccb
László Károlyi <laszlo@karolyi.hu>
2015-11-03 13:52:34 +0100

    Changing version numbering

--- build.py
+++ build.py
@@ -3,7 +3,8 @@
 
 import os
 
-from pybuilder.core import use_plugin, init
+from pybuilder.core import init, use_plugin
+from pybuilder.vcs import VCSRevision
 
 use_plugin("python.core")
 use_plugin("python.unittest")
@@ -19,7 +20,8 @@ description = """
     sns/sqs for trigger and backchannel."""
 license = 'Apache License 2.0'
 url = 'https://github.com/ImmobilienScout24/crassus'
-version = 0.1
+version = VCSRevision().get_git_revision_count()
+
 default_task = ['clean', 'analyze', 'package']
 
 
@@ -29,8 +31,11 @@ def set_properties(project):
     project.build_depends_on("unittest2")
     project.build_depends_on("mock")
     project.set_property('coverage_break_build', False)
-    project.set_property('bucket_name', os.environ.get('BUCKET_NAME_FOR_UPLOAD'))
-    project.set_property('lambda_file_access_control', os.environ.get('LAMBDA_FILE_ACCESS_CONTROL'))
+    project.set_property(
+        'bucket_name', os.environ.get('BUCKET_NAME_FOR_UPLOAD'))
+    project.set_property(
+        'lambda_file_access_control',
+        os.environ.get('LAMBDA_FILE_ACCESS_CONTROL'))
 
     project.set_property('distutils_classifiers', [
         'Development Status :: 4 - Beta',
@@ -47,8 +52,8 @@ def set_properties(project):
 def set_properties_for_teamcity_builds(project):
     project.set_property('teamcity_output', True)
 
-    project.version = '%s-%s' % (project.version,
-                                 os.environ.get('BUILD_NUMBER', 0))
+    # project.version = '%s-%s' % (project.version,
+    #                              os.environ.get('BUILD_NUMBER', 0))
     project.default_task = [
         'clean',
         'install_build_dependencies',
