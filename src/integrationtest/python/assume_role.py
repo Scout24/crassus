610de9382107cf70c9b461f68ad99efc5670a7b5
Valentin Haenel <valentin@haenel.co>
2015-11-19 15:50:01 +0100

    alternative for assuming the role

--- src/integrationtest/python/assume_role.py
+++ src/integrationtest/python/assume_role.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
-import boto3
+#import boto3
 import os
+import sys
+import json
 
 
-def assume_role():
-    invoker_role_arn = os.environ["INVOKER_ROLE_ARN"]
-    sts_client = boto3.client('sts')
-    return sts_client.assume_role(
-        RoleArn=invoker_role_arn,
-        RoleSessionName='integration_test')['Credentials']
+#def assume_role():
+#    invoker_role_arn = os.environ["INVOKER_ROLE_ARN"]
+#    sts_client = boto3.client('sts')
+#    return sts_client.assume_role(
+#        RoleArn=invoker_role_arn,
+#        RoleSessionName='integration_test')['Credentials']
 
 
 def format_(credentials):
@@ -22,4 +24,4 @@ def format_(credentials):
 
 
 if __name__ == "__main__":
-    format_(assume_role())
+    format_(json.loads(sys.stdin.read())['Credentials'])
