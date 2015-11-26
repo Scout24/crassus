55bec151ab534b86c176b9ff231f0109bfd6ceea
Stefan Neben <stefan.neben@gmail.com>
2015-11-26 15:11:20 +0100

    Fixing unit tests

---
+++ src/unittest/python/utils_tests.py
@@ -0,0 +1,36 @@
+import json
+import unittest
+
+from crassus.utils import sqs_send_message
+from crassus.deployment_response import DeploymentResponse
+from mock import patch
+
+
+class TestSqsSendMessage(unittest.TestCase):
+
+    """
+    Tests for sqs_send_message().
+    """
+
+    def setUp(self):
+        self.patch_logger = patch('crassus.utils.logger')
+        self.mock_logger = self.patch_logger.start()
+
+        self.patch_sqs = patch('crassus.utils.aws_sqs')
+        self.mock_aws_sqs = self.patch_sqs.start()
+
+    def teardown(self):
+        self.patch_logger.stop()
+        self.patch_sqs.stop()
+
+    def test_message_is_not_valid_type(self):
+        sqs_send_message(['123'], 'invalid message')
+        self.assertEqual(self.mock_logger.error.call_count, 1)
+
+    def test_message_is_valid(self):
+        message = DeploymentResponse(
+            'status', 'message', 'stack_name', 'timestamp', 'emitter')
+        message_json = json.dumps(message)
+        sqs_send_message(['123'], message)
+        self.mock_aws_sqs.send_message.assert_called_once_with(
+            QueueUrl='123', MessageBody=message_json, DelaySeconds=0)
